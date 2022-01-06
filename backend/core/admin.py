72beb0eae8dd22ec8af052cb686194bb85eb6a2f
Mitchell Anton <mitchella0100@gmail.com>
2022-01-06 18:29:32 +1100

    Created post model and super user.

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Post._meta.get_fields()]
+
+admin.site.register(Post, PostAdmin)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    id = models.TextField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    num_upvotes = models.IntegerField()
+    num_downvotes = models.IntegerField()
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    # TODO: foreign keys.
+
+    def _str_(self):
+        return self.title
