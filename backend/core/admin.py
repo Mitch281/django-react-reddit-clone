a796802d09e5b70676c001461b64fdb0c301b1bd
Mitchell Anton <mitchella0100@gmail.com>
2022-01-06 18:43:53 +1100

    Created comment and category models.'

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Post
+from .models import Category, Post, Comment
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Category._meta.get_fields()]
 
 class PostAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Post._meta.get_fields()]
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Comment._meta.get_fields()]
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- backend/core/models.py
+++ backend/core/models.py
@@ -3,15 +3,34 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Category(models.Model):
+    id = models.TextField(primary_key=True)
+    name = models.CharField(max_length=20)
+
+    def _str_(self):
+        return self.name
+
 class Post(models.Model):
     id = models.TextField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
     num_upvotes = models.IntegerField()
     num_downvotes = models.IntegerField()
     date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
-    # TODO: foreign keys.
 
     def _str_(self):
         return self.title
+
+class Comment(models.Model):
+    id = models.TextField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # We do nothing when parent post gets deleted because we still want to see the comment replies.
+    parent_post = models.ForeignKey(Post, on_delete=models.DO_NOTHING)
+
+    content = models.CharField(max_length=1000)
+    num_upvotes = models.IntegerField()
+    num_downvotes = models.IntegerField()
+    date_created = models.DateTimeField(auto_now=False, auto_now_add=True)
