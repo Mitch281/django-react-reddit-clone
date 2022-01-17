79660a0cb8195c55311aa993385f204a02c3b713
Mitchell Anton <mitchella0100@gmail.com>
2022-01-17 13:53:28 +1100

    Added permission class to get current user.

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Category, Post, Comment, PostVotes
@@ -108,6 +108,7 @@ class PostComments(viewsets.ModelViewSet):
         return queryset
 
 @api_view(["GET"])
+@permission_classes((permissions.IsAuthenticated, ))
 def current_user(request):
     """
     Determine current user by their token and return their data
