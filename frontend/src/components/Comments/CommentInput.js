90068f4ad889a1f28a5affec0233ebea079a81d6
Mitchell Anton <mitchella0100@gmail.com>
2022-01-22 21:00:38 +1100

    Added message to login or signup if not logged in and want to leave a comment.

--- frontend/src/components/Comments/CommentInput.js
+++ frontend/src/components/Comments/CommentInput.js
@@ -1,21 +1,39 @@
 import "../../style/comments.css";
 import { useContext } from "react";
 import { UserContext } from "../../App";
+import { Link } from "react-router-dom";
 
 const CommentInput = () => {
     
     const { loggedIn, usernameLoggedIn } = useContext(UserContext);
 
+    function determineOutput() {
+        if (loggedIn) {
+            return (
+            <div id="comment-input-flex-container">
+                <div id="comment-input">
+                    <span>Commenting as {usernameLoggedIn}</span>
+                    <form>
+                        <textarea />
+                        <input type="submit" value="Comment"></input>
+                    </form>
+              </div>
+            </div>
+            )
+        }
+        return (
+        <div id="comment-not-logged-in-flex-container">
+            <div id="comment-not-logged-in">
+                <span>Log in or signup to leave a comment &nbsp;</span>
+                <Link to="/login/" id="nav-to-login">Login</Link>
+                &nbsp;
+                <Link to="/signup/" id="nav-to-signup">Signup</Link>
+            </div>
+        </div>);
+    }
+
     return (
-    <div id="comment-input-flex-container">
-        <div id="comment-input">
-            <p>Commenting as {usernameLoggedIn}</p>
-            <form>
-                <textarea />
-                <input type="submit" value="Comment"></input>
-            </form>
-      </div>
-    </div>
+        determineOutput()
     );
 }
 
