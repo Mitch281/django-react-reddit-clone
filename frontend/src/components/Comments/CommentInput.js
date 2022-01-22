ba235833ccdfef4d48056b01d932cbe93d07e699
Mitchell Anton <mitchella0100@gmail.com>
2022-01-22 19:49:17 +1100

    Changed styling for comment input slightly. Also added commenting as text.

--- frontend/src/components/Comments/CommentInput.js
+++ frontend/src/components/Comments/CommentInput.js
@@ -1,13 +1,19 @@
 import "../../style/comments.css";
+import { useContext } from "react";
+import { UserContext } from "../../App";
 
 const CommentInput = () => {
+    
+    const { loggedIn, usernameLoggedIn } = useContext(UserContext);
+
     return (
     <div id="comment-input-flex-container">
-      <div id="comment-input">
-          <form>
-              <textarea />
-              <input type="submit" value="Comment"></input>
-          </form>
+        <div id="comment-input">
+            <p>Commenting as {usernameLoggedIn}</p>
+            <form>
+                <textarea />
+                <input type="submit" value="Comment"></input>
+            </form>
       </div>
     </div>
     );
