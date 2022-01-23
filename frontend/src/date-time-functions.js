dc1f926b031f7be674458aa12561af70c7c87c5b
Mitchell Anton <mitchella0100@gmail.com>
2022-01-23 16:57:11 +1100

    Fixed function for calcuating time elapsed form post.

--- frontend/src/date-time-functions.js
+++ frontend/src/date-time-functions.js
@@ -37,145 +37,57 @@ function getTimeNow() {
     return {hourNow: parseInt(hour), minuteNow: parseInt(minute), secondNow: parseInt(second)}
 }
 
-// TODO: handle case where created time bigger than time now. For example, post may have been created on 8th month, month now might be
-// 2nd month. Thus, we can't do month now - month then.
 export function getTimeElapsedFromPost(pythonDateTimeObject) {
-    let timeElapsed;
+    const dateCreated = Date.parse(pythonDateTimeObject);
+    const dateToday = new Date();
 
-    const {dayCreated, monthCreated, yearCreated} = getDayMonthYearOfPost(pythonDateTimeObject);
-    const {hourCreated, minuteCreated, secondCreated} = getTimeOfPost(pythonDateTimeObject);
-    
-    const {dayNow, monthNow, yearNow} = getDateNow();
-    const {hourNow, minuteNow, secondNow} = getTimeNow();
+    const secondsElapsed = ((dateToday.getTime() - dateCreated) / 1000);
+    const minutesElapsed = Math.round(secondsElapsed / 60);
+    const hoursElapsed = Math.round(secondsElapsed / (60 * 60));
+    const daysElapsed = Math.round(secondsElapsed / ( 60 * 60 * 24));
+    const monthsElapsed = Math.round(secondsElapsed / (60 * 60 * 24 * 31));
+    const yearsElapsed = Math.round(secondsElapsed / (60 * 60 * 24 * 31 * 12));
 
-    // Post was created in different years.
-    if (yearCreated !== yearNow) {
-        let monthsElapsed = (12 - monthCreated) + monthNow;
-
-        // Post was created more than 12 months ago. Thus, we use years.
-        if (monthsElapsed > 12) {
-            timeElapsed = yearNow - yearCreated;
-            if (timeElapsed === 1) {
-                return `${timeElapsed} year ago`;
-            } else {
-                return `${timeElapsed} years ago`;
-            }
-        
-        // Post was created less than 12 months ago. Thus, we use months.
+    if (secondsElapsed < 60) {
+        if (secondsElapsed === 1) {
+            return `${secondsElapsed} second ago`;
         } else {
-            timeElapsed = monthsElapsed;
-            if (timeElapsed === 1) {
-                return `${timeElapsed} month ago`;
-            } else {
-                return `${timeElapsed} months ago`;
-            }
+            return `${secondsElapsed} seconds ago`;
         }
     }
-
-    else if (monthCreated !== monthNow) {
-        // TODO: ADJUST FOR MOTNHS WITHOUT 31 DAYS.
-        let daysElapsed = (31 - dayCreated) + dayNow;
-
-        // Posts were created in different months, and it's been more than 31 days. Thus, we use months.
-        if (daysElapsed > 31) {
-            timeElapsed = monthNow - monthCreated;
-            if (timeElapsed === 1) {
-                return `${timeElapsed} month ago`;
-            } else {
-                return `${timeElapsed} months ago`;
-            }
-        }
-        
-        // Post was created in different months but less than or equal than 31 days apart. Thus, we use days.
-        else {
-            timeElapsed = daysElapsed;
-            if (timeElapsed === 1) {
-                return `${timeElapsed} day ago`;
-            } else {
-                return `${timeElapsed} days ago`;
-            }
+    else if (minutesElapsed < 60) {
+        if (minutesElapsed === 1) {
+            return `${minutesElapsed} minute ago`;
+        } else {
+            return `${minutesElapsed} minutes ago`;
         }
     }
-
-    // Post was created in different days.
-    else if (dayCreated !== dayNow) {
-        let hoursElapsed = (23 - hourCreated) + hourNow;
-
-        // Post was created in different days and it's been more than 24 hours. Thus, we use days.
-        if (hoursElapsed > 24) {
-            timeElapsed = dayNow - dayCreated;
-            if (timeElapsed === 1) {
-                return `${timeElapsed} day ago`;
-            } else {
-                return `${timeElapsed} days ago`;
-            }
+    else if (hoursElapsed < 24) {
+        if (hoursElapsed === 1) {
+            return `${hoursElapsed} hour ago`;
+        } else {
+            return `${hoursElapsed} hours ago`;
         }
-
-        // Post was created in different days but it's been less than 24 hours. Thus, we use hours.
-        else {
-            timeElapsed = hoursElapsed;
-            if (timeElapsed === 1) {
-                return `${timeElapsed} hour ago`;
-            } else {
-                return `${timeElapsed} hours ago`;
-            }
+    } 
+    else if (daysElapsed < 31) {
+        if (daysElapsed === 1) {
+            return `${daysElapsed} day ago`;
+        } else {
+            return `${daysElapsed} days ago`;
         }
     }
-
-    else if (hourCreated !== hourNow) {
-        let minutesElapsed = (60 - minuteCreated) + minuteNow;
-
-        // Post was created in different hours and it's been more than 60 minutes. Thus, we use hours.
-        if (minutesElapsed > 60) {
-            timeElapsed = hourNow - hourCreated;
-            if (timeElapsed === 1) {
-                return `${timeElapsed} hour ago`;
-            } else {
-                return `${timeElapsed} hours ago`;
-            }
-        }
-
-        // Post was created i different hours but it's been less than 60 minutes. Thus, we use minutes.
-        else {
-            timeElapsed = minutesElapsed;
-            if (timeElapsed === 1) {
-                return `${timeElapsed} minute ago`;
-            } else {
-                return `${timeElapsed} minutes ago`;
-            }
-        }
-    }
-
-
-    else if (minuteCreated !== minuteNow) {
-        let secondsElapsed = (60 - secondCreated) + secondNow;
-
-        // Post was created in different minutes and it's been more than 60 seconds. Thus, we use minutes.
-        if (secondsElapsed > 60) {
-            timeElapsed = minuteNow - minuteCreated;
-            if (timeElapsed === 1) {
-                return `${timeElapsed} minute ago`;
-            } else {
-                return `${timeElapsed} minutes ago`;
-            }
-        }
-
-        // Post was created in different minutes but it's been less than 60 seconds. Thus, we use seconds.
-        else {
-            timeElapsed = secondsElapsed;
-            if (timeElapsed === 1) {
-                return `${timeElapsed} seconds ago`;
-            } else {
-                return `${timeElapsed} seconds ago`;
-            }
+    else if (monthsElapsed < 12) {
+        if (monthsElapsed === 1) {
+            return `${monthsElapsed} month ago`;
+        } else {
+            return `${monthsElapsed} months ago`;
         }
     }
     else {
-        timeElapsed = secondNow - secondCreated;
-        if (timeElapsed === 1) {
-            return `${timeElapsed} seconds ago`;
+        if (yearsElapsed === 1) {
+            return `${yearsElapsed} year ago`;
         } else {
-            return `${timeElapsed} seconds ago`;
+            return `${yearsElapsed} years ago`;
         }
     }
 }
