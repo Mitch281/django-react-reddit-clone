92f2fb36d6d7dda9e8d07beda4129e787cbd7010
Mitchell Anton <mitchella0100@gmail.com>
2022-01-24 15:29:40 +1100

    Style changes for posts.

--- frontend/src/components/Home/User.js
+++ frontend/src/components/Home/User.js
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 
 const User = (props) => {
     return (
-        <span className="username">
-            Posted by {props.username} &nbsp;
+        <span>
+            Posted by <span className="username">{props.username}</span> &nbsp;
         </span>
     )
 }
