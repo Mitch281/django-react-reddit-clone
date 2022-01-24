c9b6d11aa1517a5102be380c574b3dbba60c222d
Mitchell Anton <mitchella0100@gmail.com>
2022-01-24 21:12:49 +1100

    Trigger category dropdown on click instead of on hover.

--- frontend/src/components/Nav/CategoryDropdown.js
+++ frontend/src/components/Nav/CategoryDropdown.js
@@ -1,18 +1,28 @@
 import Categories from "./Categories";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const CategoryDropdown = (props) => {
+
+    const [wantDropdown, setWantDropdown] = useState(false);
+
+    function determineDropdownDisplay() {
+        if (wantDropdown) {
+            return {display: "block"}
+        }
+        return {display: "none"}
+    }
 
     return (
         <>
             {/* This shows the category at the top of the dropdown.*/}
             <li id="top-of-dropdown">
-                <button type="button" id="category-in-focus">
+                <button type="button" id="category-in-focus" onClick={() => setWantDropdown(!wantDropdown)} >
                     {props.activeCategory === undefined ? <span>Home</span> : <span>{props.activeCategory}</span>}
                 </button>
                 {/* These are all other categories including home.*/}
-                <ul id="category-dropdown-content">
-                    <Categories />
+                <ul id="category-dropdown-content" style={determineDropdownDisplay()}>
+                    <Categories wantDropdown={wantDropdown} />
                 </ul>
             </li>
         </>
--- frontend/src/components/Nav/Navbar.js
+++ frontend/src/components/Nav/Navbar.js
@@ -3,7 +3,7 @@ import LoginButton from "./LoginButton";
 import SignupButton from "./SignupButton";
 import LogoutButton from "./LogoutButton";
 import "../../style/navbar.css";
-import { useRef, useEffect, useContext } from "react";
+import { useContext } from "react";
 import CategoryDropdown from "./CategoryDropdown";
 import { UserContext } from "../../App";
 
@@ -15,22 +15,11 @@ const Navbar = () => {
     const params = useParams();
     const activeCategory = params.categoryName;
 
-    const ref = useRef(null);
-
-    const categoryDropdownMargin = useRef();
-
-    // When component renders, we will get the width of the header. This will be used to ensure that the margin on the
-    // category dropdown is adequate enough so that it does not collide with the header.
-    useEffect(() => {
-        const headerWidth = ref.current.offsetWidth;
-        categoryDropdownMargin.current = `min(${headerWidth + 10}, 40%)`;
-    }, []);
-
     return (
         <div id="navbar">
             <ul id="navbar-nav">
                 <li id="site-name">
-                    <Link to="/" id="navbar-site-name"><h1 ref={ref}>Threddit</h1></Link>
+                    <Link to="/" id="navbar-site-name"><h1>Threddit</h1></Link>
                 </li>
                 <CategoryDropdown activeCategory={activeCategory}/>
                 <li id="navbar-auth">
