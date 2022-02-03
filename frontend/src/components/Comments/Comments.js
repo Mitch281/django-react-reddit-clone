6d2297a811a699adc9d5c9206e4e5a7ec164e34d
Mitchell Anton <mitchella0100@gmail.com>
2022-02-03 14:06:41 +1100

    Comment nesting now working!!!!!!!!!!1

--- frontend/src/components/Comments/Comments.js
+++ frontend/src/components/Comments/Comments.js
@@ -5,15 +5,26 @@ import "../../style/comments.css";
 import CommentInput from "./CommentInput";
 
 const Comments = () => {
-    console.log("rerener");
 
     const params = useParams();
     const postId = params.postId;
 
     const [comments, setComments] = useState([]);
+    const [commentsClone, setCommentsClone] = useState([]);
     const [idMapping, setIdMapping] = useState({});
     
     const [commentChain, setCommentChain] = useState([]);
+
+    // Reset the comment chain whenever our comments update. This is necessary because when a user replies to a comment or makes
+    // a comment, Comments rerenders but state does not refresh (as opposed to a page refresh where state does reset). Thus,
+    // we do not want to append to commentChain, but make the new comment chain using our new comments state.
+    useEffect(() => {
+        setCommentChain([]);
+
+        // Here, we create a deep copy of comments so that when we create our recursive JSON comment chain, we do not change
+        // comments when adding replies.
+        setCommentsClone(JSON.parse(JSON.stringify(comments)));
+    }, [comments]);
 
     async function loadComments() {
         const response = await fetch(`http://localhost:8000/api/comments/post=${postId}`);
@@ -40,25 +51,25 @@ const Comments = () => {
     }, [comments]);
 
     useEffect(() => {
-        if (idMapping !== {}) {
-            const chain = [];
-            comments.forEach((comment) => {
+        if (Object.keys(idMapping).length !== 0 && commentsClone.length !== 0) {
+            commentsClone.map((comment) => {
+
+                // Nesting level will never change so do not need to worry about it changing comments array.
                 const nestingLevel = getNestingLevel(comment, 0);
                 comment.nestingLevel = nestingLevel;
 
                 if (comment.parent_comment === null) {
-                    chain.push(comment);
+                    setCommentChain(commentChain => [...commentChain, comment]);
                     return;
                 }
 
                 // Use mapping to locate parent of comment (our loop variable) in the comments array.
-                const parentComment = comments[idMapping[comment.parent_comment]];
+                const parentComment = commentsClone[idMapping[comment.parent_comment]];
 
                 parentComment.replies = [...(parentComment.replies || []), comment];
             });
-            setCommentChain(chain);
         }
-    }, [idMapping]);
+    }, [idMapping, commentsClone]);
 
     function getNestingLevel(comment, nestingLevel) {
 
