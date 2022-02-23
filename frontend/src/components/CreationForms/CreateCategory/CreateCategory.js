e38495131685c6b1ab4d218d0a0a8eb783b20858
Mitchell Anton <mitchella0100@gmail.com>
2022-02-23 17:51:39 +1100

    Added error messages and loading indicators for creating psots and categories.

--- frontend/src/components/CreationForms/CreateCategory/CreateCategory.js
+++ frontend/src/components/CreationForms/CreateCategory/CreateCategory.js
@@ -3,12 +3,20 @@ import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../../App";
 import { v4 as uuid_v4 } from "uuid";
 import PropTypes from "prop-types";
-import { CantGetNewAccessTokenError, getNewAccessTokenIfExpired } from "../../../utils/auth";
+import {
+    CantGetNewAccessTokenError,
+    getNewAccessTokenIfExpired,
+} from "../../../utils/auth";
 import styles from "./create-category.module.css";
+import ErrorMessage from "../../ErrorMessage/ErrorMessage";
+import ClipLoader from "react-spinners/ClipLoader";
+import { constants } from "../../../constants";
 
 const CreateCategory = (props) => {
-
     const [categoryName, setCategoryName] = useState("");
+
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
 
     const { loggedIn, logout } = useContext(UserContext);
 
@@ -21,30 +29,33 @@ const CreateCategory = (props) => {
     }, []);
 
     async function createCategory() {
+        setLoading(true);
         const categoryId = uuid_v4();
         const data = {
             id: categoryId,
-            name: categoryName
-        }
+            name: categoryName,
+        };
 
         const accessToken = localStorage.getItem("accessToken");
         try {
             await getNewAccessTokenIfExpired(accessToken);
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
-            
+
         const response = await fetch("http://localhost:8000/api/categories/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${localStorage.getItem("accessToken")}`
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(data),
         });
         if (response.ok) {
             props.addCategory(data);
-            navigate(`/posts/category=${categoryName}/`, {state: {categoryId: categoryId}});
+            navigate(`/posts/category=${categoryName}/`, {
+                state: { categoryId: categoryId },
+            });
         } else {
             throw new Error(response.status);
         }
@@ -58,28 +69,59 @@ const CreateCategory = (props) => {
         }
 
         createCategory()
-        .catch(error => {
+            .catch((error) => setError(error));
+    }
 
-            // Session expired.
-            if (error instanceof CantGetNewAccessTokenError) {
-                logout();
-                navigate("/login/");
-            }
-        });
+    useEffect(() => {
+        setLoading(false);
+    }, [error]);
+
+    function getErrorMessage() {
+        if (!error) {
+            return;
+        }
+
+        // Session expired. This happens when the refresh token expires.
+        if (error instanceof CantGetNewAccessTokenError) {
+            logout();
+            navigate("/login/");
+        } else if (error.message === "400") {
+            return (
+                <ErrorMessage errorMessage="This category has already been created." />
+            );
+        }
+
+        return (
+            <ErrorMessage errorMessage="Could not create category. Please try again later." />
+        );
     }
 
     return (
         <div id={styles["create-category-flex-container"]}>
             <form onSubmit={performCreateCategory}>
-                <input type="text" value={categoryName} onChange={(e) => setCategoryName(e.target.value)} />
-                <input type="submit" value="Create Category" />
+                <input
+                    type="text"
+                    value={categoryName}
+                    onChange={(e) => setCategoryName(e.target.value)}
+                />
+                {loading ? (
+                    <ClipLoader
+                        color={constants.loaderColour}
+                        loading={true}
+                        size={20}
+                        css={"margin-top: 10px"}
+                    />
+                ) : (
+                    <input type="submit" value="Create Category" />
+                )}
+                {getErrorMessage()}
             </form>
         </div>
     );
-}
+};
 
 CreateCategory.propTypes = {
-    addCategory: PropTypes.func
-}
+    addCategory: PropTypes.func,
+};
 
 export default CreateCategory;
--- frontend/src/components/CreationForms/CreatePost/CreatePost.js
+++ frontend/src/components/CreationForms/CreatePost/CreatePost.js
@@ -3,14 +3,20 @@ import { UserContext } from "../../../App";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { v4 as uuid_v4 } from "uuid";
-import { getNewAccessTokenIfExpired, CantGetNewAccessTokenError } from "../../../utils/auth";
+import {
+    getNewAccessTokenIfExpired,
+    CantGetNewAccessTokenError,
+} from "../../../utils/auth";
 import styles from "./create-post.module.css";
+import ErrorMessage from "../../ErrorMessage/ErrorMessage";
+import ClipLoader from "react-spinners/ClipLoader";
+import { constants } from "../../../constants";
 
 const CreatePost = (props) => {
-
     let navigate = useNavigate();
 
-    const {loggedIn, usernameLoggedIn, userIdLoggedIn, logout} = useContext(UserContext);
+    const { loggedIn, usernameLoggedIn, userIdLoggedIn, logout } =
+        useContext(UserContext);
 
     useEffect(() => {
         if (!loggedIn) {
@@ -22,7 +28,11 @@ const CreatePost = (props) => {
     const [content, setContent] = useState("");
     const category = useRef(null);
 
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
+
     async function handleAddPost() {
+        setLoading(true);
 
         const postId = uuid_v4();
         const categoryId = category.current.value.split(",")[0];
@@ -40,25 +50,25 @@ const CreatePost = (props) => {
             date_created: dateNow,
             user: userIdLoggedIn,
             category: categoryId,
-        }
+        };
 
         const accessToken = localStorage.getItem("accessToken");
         try {
             await getNewAccessTokenIfExpired(accessToken);
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
         const response = await fetch("http://localhost:8000/api/posts/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${localStorage.getItem("accessToken")}`
+                Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(data),
         });
         if (response.ok) {
             props.addPost(data);
-            navigate("/"); 
+            navigate("/");
         } else {
             throw new Error(response.status);
         }
@@ -68,36 +78,75 @@ const CreatePost = (props) => {
         e.preventDefault();
 
         handleAddPost()
-        .catch(error => {
-            
-            // Session expired.
-            if (error instanceof CantGetNewAccessTokenError) {
-                logout();
-                navigate("/login/");
-            }
-        });
+            .catch((error) => setError(error));
+    }
+
+    useEffect(() => {
+        setLoading(false);
+    }, [error]);
+
+    function getErrorMessage() {
+        if (!error) {
+            return;
+        }
+
+        // Session expired (this happens when the refresh tokene expires).
+        if (error instanceof CantGetNewAccessTokenError) {
+            logout();
+            navigate("/login/");
+        }
+
+        return (
+            <ErrorMessage errorMessage="Could not create post. Please try again later." />
+        );
     }
 
     return (
         <div id={styles["create-post-flex-container"]}>
-            <form onSubmit={performAddPost} >
-                <input type="text" id={styles["post-title"]} value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
-                <textarea id={styles["post-content"]} type="text" value={content} onChange={(e) => setContent(e.target.value)} placeholder="Content" />
-                <select id={styles["select-post-category"]} ref={category}> 
-                    {props.categories.map(category => 
-                    <option key={category.id} value={`${category.id},${category.name}`}>
-                        {category.name}
-                    </option>)}
+            <form onSubmit={performAddPost}>
+                <input
+                    type="text"
+                    id={styles["post-title"]}
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="Title"
+                />
+                <textarea
+                    id={styles["post-content"]}
+                    type="text"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    placeholder="Content"
+                />
+                <select id={styles["select-post-category"]} ref={category}>
+                    {props.categories.map((category) => (
+                        <option
+                            key={category.id}
+                            value={`${category.id},${category.name}`}
+                        >
+                            {category.name}
+                        </option>
+                    ))}
                 </select>
-                <input type="submit" value="Add Post" />
+                {loading ? (
+                    <ClipLoader
+                        color={constants.loaderColour}
+                        loading={true}
+                        size={20}
+                        css={"margin-top: 10px"}
+                    />
+                ) : (
+                    <input type="submit" value="Add Post" />
+                )}
+                {getErrorMessage()}
             </form>
         </div>
     );
-}
+};
 
 CreatePost.propTypes = {
     categories: PropTypes.array,
-    addPost: PropTypes.func
-}
+    addPost: PropTypes.func,
+};
 
 export default CreatePost;
