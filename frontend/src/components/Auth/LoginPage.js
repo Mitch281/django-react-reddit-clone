1259b75ca000dcdbbc3eda0792ebb7fe09cdfc71
Mitchell Anton <mitchella0100@gmail.com>
2022-02-23 13:33:56 +1100

    Added loading indicators for logging in and signing up.

--- frontend/src/components/Auth/LoginPage.js
+++ frontend/src/components/Auth/LoginPage.js
@@ -1,31 +1,36 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../App";
 import { useNavigate } from "react-router-dom";
 import styles from "./login-signup.module.css";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import ClipLoader from "react-spinners/ClipLoader";
+import { constants } from "../../constants";
 
 const LoginPage = () => {
-
     let navigate = useNavigate();
 
-    const {setUsernameLoggedIn, setLoggedIn, setUserIdLoggedIn} = useContext(UserContext);
+    const { setUsernameLoggedIn, loggedIn, setLoggedIn, setUserIdLoggedIn } =
+        useContext(UserContext);
 
     const [error, setError] = useState("");
-    
+
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    const [loading, setLoading] = useState(false);
 
     // TODO: data validation.
     // TODO: error handling.
     // NOTE: State persists through router change but not through page refresh so handle that.
     async function handleLogin() {
+        setLoading(true);
 
         const response = await fetch("http://localhost:8000/api/token/", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify({username: username, password: password}) 
+            body: JSON.stringify({ username: username, password: password }),
         });
         if (response.ok) {
             const json = await response.json();
@@ -43,13 +48,15 @@ const LoginPage = () => {
     function performLogin(e) {
         e.preventDefault();
 
-        handleLogin()
-        .catch(error => setError(error));
+        handleLogin().catch((error) => setError(error));
     }
 
-    function getErrorMessage() {
+    useEffect(() => {
+        setLoading(false);
+    }, [error, loggedIn]);
 
-        // There is no error because the user hasn't submitted a login request yet.
+    function getErrorMessage() {
+        // There is no error because the user hasn't submitted a login request yet or the request was successful.
         if (!error) {
             return;
         }
@@ -57,14 +64,18 @@ const LoginPage = () => {
         if (error.message === "401") {
             return (
                 <div id={styles["login"]}>
-                    <ErrorMessage errorMessage={"Wrong username or password."} />
+                    <ErrorMessage
+                        errorMessage={"Wrong username or password."}
+                    />
                 </div>
             );
         }
-        
+
         return (
             <div id={styles["login"]}>
-                <ErrorMessage errorMessage={"Couldn't login. Please try again later"} />
+                <ErrorMessage
+                    errorMessage={"Couldn't login. Please try again later"}
+                />
             </div>
         );
     }
@@ -73,14 +84,33 @@ const LoginPage = () => {
         <>
             <div id={styles["login"]}>
                 <form onSubmit={performLogin}>
-                    <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <input type="submit" value="login" />
+                    <input
+                        type="text"
+                        placeholder="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {loading ? (
+                        <ClipLoader
+                            color={constants.loaderColour}
+                            loading={true}
+                            size={20}
+                            css={"margin-top: 10px"}
+                        />
+                    ) : (
+                        <input type="submit" value="Login" />
+                    )}
                 </form>
             </div>
             {getErrorMessage()}
         </>
-    )
-}
+    );
+};
 
-export default LoginPage
+export default LoginPage;
--- frontend/src/components/Auth/SignupPage.js
+++ frontend/src/components/Auth/SignupPage.js
@@ -1,14 +1,16 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../App";
 import styles from "./login-signup.module.css";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import ClipLoader from "react-spinners/ClipLoader";
+import { constants } from "../../constants";
 
 const SignupPage = () => {
-
     let navigate = useNavigate("/");
 
-    const {setUsernameLoggedIn, setLoggedIn, setUserIdLoggedIn} = useContext(UserContext);
+    const { setUsernameLoggedIn, loggedIn, setLoggedIn, setUserIdLoggedIn } =
+        useContext(UserContext);
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -16,8 +18,11 @@ const SignupPage = () => {
 
     const [error, setError] = useState("");
 
+    const [loading, setLoading] = useState(false);
+
     async function handleSignup() {
-        
+        setLoading(true);
+
         if (password !== confirmPassword) {
             throw new Error("Passwords not the same");
         }
@@ -25,9 +30,9 @@ const SignupPage = () => {
         const response = await fetch("http://localhost:8000/api/users/", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify({username: username, password: password})
+            body: JSON.stringify({ username: username, password: password }),
         });
         if (response.ok) {
             const json = await response.json();
@@ -45,33 +50,39 @@ const SignupPage = () => {
     function performSignup(e) {
         e.preventDefault();
 
-        handleSignup()
-        .catch(error => setError(error));
+        handleSignup().catch((error) => setError(error));
     }
+
+    useEffect(() => {
+        setLoading(false);
+    }, [error, loggedIn]);
 
     function getErrorMessage() {
         if (!error) {
             return;
         }
-        
+
         if (error.message === "400") {
             return (
                 <div id={styles["login"]}>
                     <ErrorMessage errorMessage={"Username already in use!"} />
                 </div>
             );
-        }
-        else if (error.message === "Passwords not the same") {
+        } else if (error.message === "Passwords not the same") {
             return (
                 <div id={styles["signup"]}>
-                    <ErrorMessage errorMessage={"Passwords entered are not the same!"} />
+                    <ErrorMessage
+                        errorMessage={"Passwords entered are not the same!"}
+                    />
                 </div>
             );
         }
 
         return (
             <div id={styles["signup"]}>
-                <ErrorMessage errorMessage={"Could't signup. Please try again later."} />
+                <ErrorMessage
+                    errorMessage={"Could't signup. Please try again later."}
+                />
             </div>
         );
     }
@@ -80,17 +91,40 @@ const SignupPage = () => {
         <>
             <div id={[styles["signup"]]}>
                 <form onSubmit={performSignup}>
-                    <input type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <input type="password" placeholder="confirm password" value={confirmPassword} 
-                    onChange={(e) => setConfirmPassword(e.target.value)} />
-                    
-                    <input type="submit" value="Signup" />
+                    <input
+                        type="text"
+                        placeholder="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="confirm password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+
+                    {loading ? (
+                        <ClipLoader
+                            color={constants.loaderColour}
+                            loading={true}
+                            size={20}
+                            css={"margin-top: 10px"}
+                        />
+                    ) : (
+                        <input type="submit" value="Signup" />
+                    )}
                 </form>
             </div>
             {getErrorMessage()}
         </>
-    )
-}
+    );
+};
 
-export default SignupPage
+export default SignupPage;
