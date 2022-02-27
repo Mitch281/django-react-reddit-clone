2bfcc4999dacf094c7997e381f7157e68a5f38e4
Mitchell Anton <mitchella0100@gmail.com>
2022-02-27 14:40:27 +1100

    Got rid of close button on error message.

--- frontend/src/components/ErrorMessage/ErrorMessage.js
+++ frontend/src/components/ErrorMessage/ErrorMessage.js
@@ -1,32 +1,16 @@
 // We will show this componenet whenever something goes wrong and it is most likely due to a server error.
 
-import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./error-message.module.css";
 import { BiErrorCircle } from "react-icons/bi";
 
 const ErrorMessage = (props) => {
 
-    const [wantErrorMessage, setWantErrorMessage] = useState(false);
-
-    useEffect(() => {
-        setWantErrorMessage(true);
-    }, []);
-
-    function closeErrorMessage() {
-        setWantErrorMessage(false);
-    }
-
     return (
-        <>
-            {wantErrorMessage ?         
-                <div id={styles["error-message"]}>
-                    <BiErrorCircle />
-                    <span>{props.errorMessage}</span>
-                    <button type="button" onClick={closeErrorMessage}>X</button>
-                </div> : ""
-            }
-        </>
+        <div id={styles["error-message"]}>
+            <BiErrorCircle />
+            <span>{props.errorMessage}</span>
+        </div>
     );
 }
 
