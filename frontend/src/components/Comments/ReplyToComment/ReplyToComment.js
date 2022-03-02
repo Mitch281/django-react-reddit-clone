07cd502e6d1568ecf2c973e1340f412f3f7da9ec
Mitchell Anton <mitchella0100@gmail.com>
2022-03-02 14:09:37 +1100

    Added loading indicators and error messages for replying to comments.

--- frontend/src/components/Comments/ReplyToComment/ReplyToComment.js
+++ frontend/src/components/Comments/ReplyToComment/ReplyToComment.js
@@ -1,31 +1,34 @@
-import { useState, useContext } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { UserContext } from "../../../App";
 import PropTypes from "prop-types";
+import { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
 import { v4 as uuid_v4 } from "uuid";
-import { getNewAccessTokenIfExpired, CantGetNewAccessTokenError } from "../../../utils/auth";
-import styles from "./reply-to-comment.module.css";
+import { UserContext } from "../../../App";
+import { constants } from "../../../constants";
+import { CantGetNewAccessTokenError } from "../../../utils/auth";
 import { postReplyToComment } from "../../../utils/fetch-data";
+import ErrorMessageModal from "../../ErrorMessage/ErrorMessageModal";
+import styles from "./reply-to-comment.module.css";
 
 const ReplyToComment = (props) => {
-
-    let navigate = useNavigate();
-
     const [replyContent, setReplyContent] = useState("");
-    const { usernameLoggedIn, userIdLoggedIn, logout } = useContext(UserContext);
+    const { usernameLoggedIn, userIdLoggedIn } =
+        useContext(UserContext);
+
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
 
     const params = useParams();
     const postId = params.postId;
 
     function getDisplay() {
         if (!props.wantReplyForm) {
-            return {display: "none"}
+            return { display: "none" };
         }
     }
 
     // TODO: data validation.
     async function postReply() {
-
         const dateNow = new Date().toString();
 
         const commentId = uuid_v4();
@@ -40,8 +43,8 @@ const ReplyToComment = (props) => {
             date_created: dateNow,
             parent_comment: props.parentCommentId,
             hidden: false,
-            num_replies: 0
-        }
+            num_replies: 0,
+        };
 
         try {
             await postReplyToComment(reply);
@@ -50,44 +53,82 @@ const ReplyToComment = (props) => {
 
             props.toggleReplyForm();
             props.updateComments(reply);
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
     }
 
-    function performPostReply(e) {
+    async function performPostReply(e) {
         e.preventDefault();
+        setLoading(true);
 
-        postReply()
-        .catch(error => {
-            
-            // Session expired.
-            if (error instanceof CantGetNewAccessTokenError) {
-                logout();
-                navigate("/login/");
-            }
-        });
+        try {
+            await postReply();
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    function getErrorMessage() {
+        if (!error) {
+            return;
+        }
+
+        if (error instanceof CantGetNewAccessTokenError) {
+            return (
+                <ErrorMessageModal errorMessage="Session expired. Please login again." />
+            );
+        }
+
+        return (
+            <ErrorMessageModal errorMessage="Could not reply to comment. Please try again later." />
+        );
     }
 
     return (
-        <div style={getDisplay()}>
-            <form className={styles["reply-to-comment-form"]} onSubmit={performPostReply}>
-                <div>
-                    <span>Reply to {props.parentUsername} as {usernameLoggedIn}</span>
-                    <textarea type="text" value={replyContent} onChange={(e) => setReplyContent(e.target.value)} placeholder="Content" />
-                    <input type="submit" value="Reply" />
-                </div>
-            </form>        
-        </div>
+        <>
+            <div style={getDisplay()}>
+                <form
+                    className={styles["reply-to-comment-form"]}
+                    onSubmit={performPostReply}
+                >
+                    <div>
+                        <span>
+                            Reply to {props.parentUsername} as{" "}
+                            {usernameLoggedIn}
+                        </span>
+                        <textarea
+                            type="text"
+                            value={replyContent}
+                            onChange={(e) => setReplyContent(e.target.value)}
+                            placeholder="Content"
+                        />
+                        {loading ? (
+                            <ClipLoader
+                                color={constants.loaderColour}
+                                loading={true}
+                                size={20}
+                                css={"margin-top: 10px"}
+                            />
+                        ) : (
+                            <input type="submit" value="Reply" />
+                        )}
+                    </div>
+                </form>
+            </div>
+            {getErrorMessage()}
+        </>
     );
-}
+};
 
 ReplyToComment.propTypes = {
     wantReplyForm: PropTypes.bool,
     parentCommentId: PropTypes.string,
     parentUsername: PropTypes.string,
     updateComments: PropTypes.func,
-    togglyReplyForm: PropTypes.func
-}
+    togglyReplyForm: PropTypes.func,
+};
 
 export default ReplyToComment;
