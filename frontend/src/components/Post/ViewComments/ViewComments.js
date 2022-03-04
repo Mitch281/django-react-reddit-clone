176e6eb22ef4f980bd77eb58acd6c89d9a85a216
Mitchell Anton <mitchella0100@gmail.com>
2022-03-04 21:02:28 +1100

    Added error handling and loading for number of comments.

--- frontend/src/components/Post/ViewComments/ViewComments.js
+++ frontend/src/components/Post/ViewComments/ViewComments.js
@@ -3,36 +3,75 @@ import { useState, useEffect } from "react";
 import { BiCommentDetail } from "react-icons/bi";
 import { fetchNumberOfCommentsOnPost } from "../../../utils/fetch-data";
 import styles from "./view-comments.module.css";
+import ClipLoader from "react-spinners/ClipLoader";
+import { constants } from "../../../constants";
+import { BsFillExclamationCircleFill } from "react-icons/bs";
 
 const ViewComments = (props) => {
-
     const [numComments, setNumComments] = useState();
-    
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
+
     async function getNumberOfComments() {
         try {
             const json = await fetchNumberOfCommentsOnPost(props.postId);
             setNumComments(json.num_comments);
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
     }
 
-    useEffect(() => {
-        getNumberOfComments()
-        .catch(error => console.log(error));
+    useEffect(async () => {
+        try {
+            await getNumberOfComments();
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
     }, []);
 
-    return ( 
-        <div className={styles["nav-to-comments"]} onClick={() => props.navigateToPost(props.postId)}>
-            <BiCommentDetail />
-            <span>{numComments} {numComments === 1 ? "Comment" : "Comments"}</span>
+    function getOutput() {
+        if (error) {
+            return (
+                <BsFillExclamationCircleFill
+                    color={constants.errorMessageColour}
+                />
+            );
+        } else if (loading) {
+            return (
+                <ClipLoader
+                    color={constants.loaderColour}
+                    loading={true}
+                    size={20}
+                    css={"margin-top: 10px"}
+                />
+            );
+        }
+
+        return (
+            <>
+                <BiCommentDetail />
+                <span>
+                    {numComments} {numComments === 1 ? "Comment" : "Comments"}
+                </span>
+            </>
+        );
+    }
+
+    return (
+        <div
+            className={styles["nav-to-comments"]}
+            onClick={() => props.navigateToPost(props.postId)}
+        >
+            {getOutput()}
         </div>
     );
-}
+};
 
 ViewComments.propTypes = {
     postId: PropTypes.string,
-    navigateToPost: PropTypes.func
-}
+    navigateToPost: PropTypes.func,
+};
 
 export default ViewComments;
