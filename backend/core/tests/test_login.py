0cffa83e4e9661304a41c0f0a56da199a792bcd8
Mitch281 <mitchella0100@gmail.com>
2023-09-20 19:00:00 +1000

    Unit testing for logging in.

---
+++ backend/core/tests/test_login.py
@@ -0,0 +1,52 @@
+from core.models import User
+from django.contrib.auth.hashers import make_password
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+USER_ID_OF_TEST_USER = 1
+
+class TestLogin(APITestCase):
+    def setUp(self):
+        test_user = User(id=USER_ID_OF_TEST_USER, username='test')
+            # Since we are not overriding our user model and extending the AbstractUser class, we use this instead of user.set_password('test')
+        test_user.password = make_password('test')
+        test_user.save()
+
+    def test_login(self):
+        url = reverse('core:token_obtain_pair')
+        body = {
+            'username': 'test',
+            'password': 'test'
+        }
+        response = self.client.post(url, body, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.data
+        self.assertEqual(data['refresh'][0: 2] == 'ey', True)
+        self.assertEqual(data['access'][0: 2] == 'ey', True)
+        self.assertEqual(data['user_id'], USER_ID_OF_TEST_USER)
+
+    def test_username_does_not_exist(self):
+        url = reverse('core:token_obtain_pair')
+        body = {
+            'username': 'does not exist',
+            'password': 'test'
+        }
+        response = self.client.post(url, body, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        data = response.data
+        self.assertEqual(data['detail'], 'The username or password does not match.')
+
+    def test_wrong_password(self):
+        url = reverse('core:token_obtain_pair')
+        body = {
+            'username': 'test',
+            'password': 'wrong password'
+        }
+        response = self.client.post(url, body, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        data = response.data
+        self.assertEqual(data['detail'], 'The username or password does not match.')
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, re_path
-from core import views
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
+app_name = 'core'
 
-from core.views import PostVotingViewSet, CommentVotingViewSet
+from core import views
+from core.views import CommentVotingViewSet, PostVotingViewSet
+from django.urls import path, re_path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'post', PostVotingViewSet, basename='post')
@@ -13,38 +12,47 @@ router.register(r'comment', CommentVotingViewSet, basename="comment")
 
 urlpatterns = [
     # See https://www.django-rest-framework.org/api-guide/viewsets/ for good documenation as_view params.
-    path('categories/', views.CategoryView.as_view(), name='categories'),
+    path('categories/', views.CategoryView.as_view(), name='all categories'),
 
-    path("posts/", views.PostsView.as_view(), name="posts"),
+    path("posts/", views.PostsView.as_view(), name='all posts'),
+
     re_path(r'^posts/(?P<ordering>[a-zA-Z\s]*)/$',
-            views.PostsView.as_view(), name='posts'),
+            views.PostsView.as_view(), name='posts by order'),
+
     re_path(r'^posts/(?P<ordering>[a-zA-Z\s]*)?limit=(?P<limit>[0-9]*)&page-number=(?P<page_number>[0-9]*)/$',
-            views.PostsView.as_view(), name='posts'),
+            views.PostsView.as_view(), name='posts by order with page number'),
+
     re_path(
-        r'^posts?limit=(?P<limit>[0-9]*)&page-number=(?P<page_number>[0-9]*)/$', views.PostsView.as_view()),
+        r'^posts?limit=(?P<limit>[0-9]*)&page-number=(?P<page_number>[0-9]*)/$', views.PostsView.as_view(), name='all posts with page number'),
 
     re_path(r'^posts/category/(?P<pk>[0-9a-z-&]+)/$',
-            views.PostsByCategoryView.as_view()),
-    re_path(r'^posts/category/(?P<pk>[0-9a-z-&]+)?limit=(?P<limit>[0-9]*)&page-number=(?P<page_number>[0-9]*)/$',
-            views.PostsByCategoryView.as_view()),
-    re_path(r'^posts/category/(?P<pk>[0-9a-z-&]+)/(?P<ordering>[a-zA-Z\s]*)/$',
-            views.PostsByCategoryView.as_view()),
-    re_path(r'^posts/category/(?P<pk>[0-9a-z-&]+)/(?P<ordering>[a-zA-Z\s]*)?limit=(?P<limit>[0-9]*)&page-number=(?P<page_number>[0-9]*)/$',
-            views.PostsByCategoryView.as_view()),
+            views.PostsByCategoryView.as_view(), name='posts by category'),
 
-    re_path(r'^comment/(?P<pk>[0-9a-z-&]+)/$', views.CommentView.as_view()),
+    re_path(r'^posts/category/(?P<pk>[0-9a-z-&]+)?limit=(?P<limit>[0-9]*)&page-number=(?P<page_number>[0-9]*)/$',
+            views.PostsByCategoryView.as_view(), name='posts by category and page number'),
+
+    re_path(r'^posts/category/(?P<pk>[0-9a-z-&]+)/(?P<ordering>[a-zA-Z\s]*)/$',
+            views.PostsByCategoryView.as_view(), name='posts by category and order'),
+
+    re_path(r'^posts/category/(?P<pk>[0-9a-z-&]+)/(?P<ordering>[a-zA-Z\s]*)?limit=(?P<limit>[0-9]*)&page-number=(?P<page_number>[0-9]*)/$',
+            views.PostsByCategoryView.as_view(), name='posts by category, order, and page number'),
+
+    re_path(r'^comment/(?P<pk>[0-9a-z-&]+)/$', views.CommentView.as_view(), name='comment by id'),
+
     re_path(
         r'^comment/(?P<pk>[0-9a-z-&]+)?user-id=(?P<user_id>[0-9a-z-&]+)/$', views.CommentView.as_view()),
-    path('comments/', views.CommentsView.as_view(), name='comments'),
+    path('comments/', views.CommentsView.as_view(), name='all comments'),
 
     re_path(
-        r'^post/(?P<parent_post_id>[0-9a-z-&]+)/comments/$', views.PostComments.as_view()),
-    re_path(
-        r'^post/(?P<parent_post_id>[0-9a-z-&]+)/comments/(?P<ordering>[a-zA-Z\s]*)/$', views.PostComments.as_view()),
+        r'^post/(?P<parent_post_id>[0-9a-z-&]+)/comments/$', views.PostComments.as_view(), name='comments on post'),
 
-    re_path(r'^post/(?P<pk>[0-9a-z-&]+)/$', views.PostView.as_view()),
     re_path(
-        r'^post/(?P<pk>[0-9a-z-&]+)?user-id=(?P<user_id>[0-9a-z-&]+)/$', views.PostView.as_view()),
+        r'^post/(?P<parent_post_id>[0-9a-z-&]+)/comments/(?P<ordering>[a-zA-Z\s]*)/$', views.PostComments.as_view(), name='comments on post by order'),
+
+    re_path(r'^post/(?P<pk>[0-9a-z-&]+)/$', views.PostView.as_view(), name='post by id'),
+
+    re_path(
+        r'^post/(?P<pk>[0-9a-z-&]+)?user-id=(?P<user_id>[0-9a-z-&]+)/$', views.PostView.as_view(), name='post by id and user id'),
 
     path('post-votes/', views.PostVotesView.as_view()),
 
