e48c485e678debf5c73dffa9da0ad84b9a55ae49
Mitch281 <mitchella0100@gmail.com>
2023-11-16 22:55:45 +1100

    Fix collectstatic bug, and change api endpoint on frontend to new aws api gateway

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -183,9 +183,9 @@ os.makedirs(STATIC_TMP, exist_ok=True)
 
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 SIMPLE_JWT = {
